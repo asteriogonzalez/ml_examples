fixing backprop

aiml.py
@@ -285,11 +285,182 @@ def  solve_logistic_regression(X, y, theta=None, learning_rate=1.0):
         x0=theta,
         args=(X, y, learning_rate),
         # method='TNC',
-        # method='L-BFGS-B',
-        method='BFGS',
+        # method='SLSQP',
+        method='L-BFGS-B',
+        # method='BFGS',
         jac=grad,
         callback=retail,
+        # options={'maxiter': 100000, 'disp': False}
     )
 
     theta = result.x
     return theta, result
+
+
+class FNN(object):
+    def __init__(self, sizes=None):
+        self.Theta = list()
+        self.Z = list()  # intermediate output values for each layer
+
+        if sizes:
+            self.create_netwotk(sizes)
+
+    def create_netwotk(self, sizes):
+        """Create the network structure.
+        sizes contains the number of nodes in each layer.
+
+        size[0] is the number of input features, without bias term
+        size[1] is the number of nodes (sub-features) witous bias
+        ...
+        size[n] is the number of nodes at the end, usually match with
+                the number of classes in classification problems.
+        """
+        self.Theta = list()
+        for i in range(0, len(sizes) - 1):
+            # use extended sizes with bias term for convenience
+            # but remove in the last stage
+            size = (sizes[i] + 1, sizes[i+1] + 1)
+            # create and initialize layer with random values
+            # following the advice from Andre Ng
+            epsilon = np.sqrt(6) / np.sqrt(sum(size))
+            Theta = np.random.randn(*size) * epsilon
+            self.Theta.append(Theta)
+
+        # remove the bias term in the last stage, as we
+        # haven't anything to compare with
+        self.Theta[-1] = Theta[:, 1:]
+
+        self.Z = [None] * (len(self.Theta) + 1)
+
+    def solve(self, X, y):
+        # setup optimization params
+        klasses = np.unique(y)
+        n_klasses = klasses.size
+        klasses = range(n_klasses)
+        samples, features = X.shape
+        y.shape = (samples, )
+
+        # setup the Y multiclass matrix
+        Y = np.zeros((samples, n_klasses), dtype=np.int8)
+        for i, klass in enumerate(y):
+            Y[i][klass - 1] = 1
+
+        Z = X.copy()
+        # include the bias term in previous step
+        ones = np.ones((Z.shape[0], 1))
+        Z = np.c_[ones, Z]
+        self.Z[0] = Z
+
+        errors = []
+        deltas = []
+
+        alpha = 5
+        for iteration in xrange(150):
+            self.forward()
+            self._backprop(Y, alpha)
+
+            J = self.Z[-1] - Y
+            J = (J * J).mean()
+
+            if errors and errors[-1] < J:
+                print "Reducing alpha: %f" % alpha
+                alpha /= 2.0
+            # else:
+                # alpha *= 1.1
+            errors.append(J)
+
+            delta = np.mean([d.mean() for d in self.Z])
+            deltas.append(delta)
+
+            print "[%3d] Cost: J=%f, delta=%f, alpha=%f" % \
+            (iteration, J, delta, alpha)
+
+            if delta < 1e-3 or J < 1e-5:
+                print "-End-"
+                break
+
+
+        plt.plot(errors)
+        plt.show()
+        plt.plot(deltas)
+        plt.show()
+
+        p = self.predict(X)
+        print "Accurracy: %f" % (p == y).mean()
+
+        foo = 1
+
+    def forward(self):
+        # Note  that each Theta is a matrix with the bias term included.
+        for i, Theta in enumerate(self.Theta):
+            # make the logistic regression for this layer.
+            Z = self.Z[i]
+            Z[:, 0] = 1
+            #Z = Z.dot(Theta)
+            Z = np.einsum('ab,bc->ac', Z, Theta)
+            Z = sigmoid(Z)
+            self.Z[i + 1] = Z
+        return Z
+
+    def _backprop(self, Y, alpha=1, lam=0.01):
+        batch, n_klasses = Y.shape
+
+        Z = self.Z[-1]
+        delta = Z -Y
+        der = 1
+        for i in range(len(self.Theta) - 1, -1, -1):
+            Z = self.Z[i]
+            # note we adimensionalize with batch size * n clases
+            dTheta = np.einsum('ki,kj->ij', Z, delta) / Y.size
+
+            # print 1, dTheta.mean()
+
+            # regularization parameter
+            # note we only adimensionalize with n classes
+            # as Theta don't depend on batch size
+            Theta = self.Theta[i]
+            reg = np.einsum('ab,ab->ab', Theta, Theta) / n_klasses
+            reg[0] = 0  # don't regularize bias param
+
+            # print 2, reg.mean()
+
+            dTheta += lam * reg
+
+            # update Theta
+            self.Theta[i] -= alpha * dTheta
+
+            # propagate delta
+            der = self.derivate(i + 1)
+            # delta = delta * der * Theta
+            delta = np.einsum('nj,ij->ni', delta*der, Theta)
+
+        foo = 1
+
+    def derivate(self, i):
+        "In the sigmoid case, is a straight value"
+        Z = self.Z[i]
+        return Z * (1 - Z)
+
+    def predict(self, X):
+        Z = np.insert(X, 0, values=np.ones(X.shape[0]), axis=1)
+        for i, Theta in enumerate(self.Theta):
+            # make the logistic regression for this layer.
+            Z[:, 0] = 1
+            Z = np.einsum('ab,bc->ac', Z, Theta)
+            Z = sigmoid(Z)
+
+        predict = []
+        for sample in Z:
+            i = np.argmax(sample)
+            # p = sample[i]
+            predict.append(i)
+        return predict
+
+    def cost(self, X, y):
+        p = self.predict(X)
+        e = p - y
+        return (e * e).mean()
+
+
+
+

ex3.py
@@ -18,124 +18,9 @@ from scipy.io import loadmat
 
 from aiml import *
 
+FILE_SEED = 'Theta.npy'
 
-def rebuilt(X, theta):
-    """Reshape the Theta tensor in single vector suitable for
-    optimization algorithms"""
-    features = X.shape[1]
-    klasses = theta.size // features
-
-    assert features * klasses == theta.size  # perfect fitting
-
-    Theta = np.reshape(theta, (features, klasses))
-    return Theta
-
-
-def cost(theta, X, Y, lam=0):
-    "Compute the cost of logistic regression"
-    n = Y.shape[0]
-    Theta = rebuilt(X, theta)
-    h = sigmoid(np.dot(X, Theta))
-
-    cost = -Y * np.log(h) - (1 - Y) * np.log(1 - h)
-    cost = cost.sum()
-    if lam > 0:  # regularization ignore bias parameter
-        theta2 = np.copy(theta)
-        theta2[0] = 0
-        reg = lam * np.dot(theta2, theta2) / 2.0
-        cost += reg
-
-    cost /= n
-
-    print "COST:", cost
-    return cost
-
-
-def grad(theta, X, Y, lam=0):
-    "Compute the gradient of logistic regression."
-    n = Y.shape[0]
-
-    Theta = rebuilt(X, theta)
-    H = sigmoid(np.dot(X, Theta))
-
-    d = H - Y
-    grad = X.T.dot(d)
-
-    if lam > 0:  # regularization ignore bias parameter
-        # theta2 = np.zeros_like(theta)
-        # theta2[1:] = theta[1:]
-        Theta2 = np.copy(Theta)
-        Theta2[0] = 0
-        reg = lam * Theta2
-        grad += reg
-
-    grad /= n
-
-    # restore same shape that minimization algorithms uses
-    grad.shape = theta.shape
-    return grad
-
-
-def multiclass_logistic_regression():
-    "Make a simple linear regression from from Andre Ng Exercise 1"
-    # Load data from CSV
-    data = loadmat('ex3data1.mat')
-
-    # X: 5000 samples x 400 pixels per image (aka features)
-    # y: 5000 class belonging vector from 1 to 10 (1-index based!)
-    # so in this example, K=10
-    X, y = data['X'], data['y']
-
-    # just take a simple random sample and draw it
-    # selected = random.randint(0, X.shape[0])
-    # image = X[selected].reshape((20, 20))
-    # plt.imshow(image, cmap='gray')
-    # plt.title('Should be a %s' % y[selected])
-    # plt.show()
-
-    # setup optimization params
-    klasses = np.unique(y)
-    n_klasses = klasses.size
-    samples, features = X.shape
-
-    # Theta is now a matrix of (klasses x features)
-    # don't forget the bias term for each klass
-    Theta = np.random.randn(1 + features, n_klasses)
-    Theta[0] = 1  # the bias term
-    X = np.insert(X, 0, values=np.ones(samples), axis=1)
-    # setup the Y multiclass matrix
-    Y = np.zeros((samples, n_klasses), dtype=np.int8)
-    for i, klass in enumerate(y):
-        Y[i][klass - 1] = 1
-
-    learning_rate = 0.1
-    # save theta evolution
-    trajectory = []
-
-    def retail(x):
-        "save each step of the training"
-        print x
-        trajectory.append(x.copy())
-
-    # call optimization method
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
-    result = minimize(
-        fun=cost,
-        x0=Theta,
-        args=(X, Y, learning_rate),
-        # method='TNC',
-        method='BFGS',  # L-BFGS-B
-        jac=grad,
-        callback=retail,
-    )
-
-    # Train accuracy
-    p = predict_multi_class(Theta, X)
-    p += 1   # y is 1-based index in the examples!
-    print "Train Accuracy: %f" % (p == y).mean()
-
-
-def multiclass_logistic_regression_split():
+def multiclass_logistic_regression_k_problems(load_seed=True):
     "Make a simple linear regression from from Andre Ng Exercise 1"
     # Load data from CSV
     data = loadmat('ex3data1.mat')
@@ -170,9 +55,8 @@ def multiclass_logistic_regression_split():
     learning_rate = 0.1
 
     # check if we have solved the problem before, just for fast debugging
-    path = 'Theta.npy'
-    if os.path.exists(path):
-        Theta = np.load(path)
+    if load_seed and os.path.exists(FILE_SEED):
+        Theta = np.load(FILE_SEED)
     else:
         for k, klass in enumerate(klasses):
             y_i = (y == (k + 1)) * 1
@@ -181,7 +65,7 @@ def multiclass_logistic_regression_split():
                 X, y_i, theta, learning_rate)
             Theta[:, k] = theta
             print "Class %s : error: %s, %s" % (k, result.fun, result.message)
-        np.save(path, Theta)
+        np.save(FILE_SEED, Theta)
 
     # Train accuracy
     p = predict_multi_class(Theta, X)
@@ -189,6 +73,176 @@ def multiclass_logistic_regression_split():
     print "Train Accuracy: %f" % (p == y).mean()
 
 
+def multiclass_logistic_regression_single_problem(load_seed=True):
+    """Make a logistic regression building a single problem
+    and trying to solve as a whole.
+
+    load_seed: if True, try to load the Theta solution from previous run.
+    """
+
+    def rebuilt(X, theta):
+        """Reshape the Theta tensor in single vector suitable for
+        optimization algorithms"""
+        features = X.shape[1]
+        klasses = theta.size // features
+
+        assert features * klasses == theta.size  # perfect fitting
+
+        Theta = np.reshape(theta, (features, klasses))
+        return Theta
+
+
+    def cost(theta, X, Y, lam=0):
+        "Compute the cost of logistic regression"
+        Theta = rebuilt(X, theta)
+        h = sigmoid(np.dot(X, Theta))
+
+        cost = -Y * np.log(h) - (1 - Y) * np.log(1 - h)
+        cost = cost.sum()
+        if lam > 0:  # regularization ignore bias parameter
+            theta2 = np.copy(theta)
+            theta2[0] = 0
+            reg = lam * np.dot(theta2, theta2) / 2.0
+            cost += reg
+
+        cost /= Y.shape[0]
+        return cost
+
+
+    def grad(theta, X, Y, lam=0):
+        "Compute the gradient of logistic regression."
+        n = Y.shape[0]
+
+        Theta = rebuilt(X, theta)
+        H = sigmoid(np.dot(X, Theta))
+
+        d = H - Y
+        grad = X.T.dot(d)
+
+        if lam > 0:  # regularization ignore bias parameter
+            # theta2 = np.zeros_like(theta)
+            # theta2[1:] = theta[1:]
+            Theta2 = np.copy(Theta)
+            Theta2[0] = 0
+            reg = lam * Theta2
+            grad += reg
+
+        grad /= n
+
+        # restore same shape that minimization algorithms uses
+        grad.shape = theta.shape
+        return grad
+
+
+    # Load data from CSV
+    data = loadmat('ex3data1.mat')
+
+    # X: 5000 samples x 400 pixels per image (aka features)
+    # y: 5000 class belonging vector from 1 to 10 (1-index based!)
+    # so in this example, K=10
+    X, y = data['X'], data['y']
+
+    # setup optimization params
+    klasses = np.unique(y)
+    n_klasses = klasses.size
+    samples, features = X.shape
+
+    # Theta is now a matrix of (klasses x features)
+    # don't forget the bias term for each klass
+    Theta = np.random.randn(1 + features, n_klasses)
+    Theta[0] = 1  # the bias term
+    X = np.insert(X, 0, values=np.ones(samples), axis=1)
+    y.shape = (samples, )
+
+    # setup the Y multiclass matrix
+    Y = np.zeros((samples, n_klasses), dtype=np.int8)
+    for i, klass in enumerate(y):
+        Y[i][klass - 1] = 1
+
+    learning_rate = 0.1
+
+    # save theta evolution
+    trajectory = []
+
+    def retail(theta):
+        "save each step of the training"
+        # Theta = rebuilt(X, theta)
+        # p = predict_multi_class(Theta, X)
+        # p += 1   # y is 1-based index in the examples!
+        # print "Train Accuracy: %f" % (p == y).mean()
+        trajectory.append(theta.copy())
+
+    if load_seed and os.path.exists(FILE_SEED):
+        Theta = np.load(FILE_SEED)
+
+    shape = Theta.shape
+
+    # call optimization method
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html
+    result = minimize(
+        fun=cost,
+        x0=Theta,
+        args=(X, Y, learning_rate),
+        # method='TNC',
+        # method='SLSQP',
+        method='L-BFGS-B',
+        # method='BFGS',  # L-BFGS-B
+        jac=grad,
+        callback=retail,
+    )
+
+    Theta = result.x
+    Theta.shape = shape
+    # Train accuracy
+    p = predict_multi_class(Theta, X)
+    p += 1   # y is 1-based index in the examples!
+    print "Train Accuracy: %f" % (p == y).mean()
+
+def multiclass_with_FNN(load_seed=True):
+    "Similar to Logistic Regression, but using FNN to solve the problem"
+
+    # the code is pretty similar
+    data = loadmat('ex3data1.mat')
+
+    # X: 5000 samples x 400 pixels per image (aka features)
+    # y: 5000 class belonging vector from 1 to 10 (1-index based!)
+    # so in this example, K=10
+    X, y = data['X'], data['y']
+
+    # y[y==10] = 0  # translate label, I don't like 1-based indexes :)
+    samples, features = X.shape
+
+    # setup optimization params
+    klasses = np.unique(y)
+    n_klasses = klasses.size
+    y.shape = (samples, )
+
+    learning_rate = 0.1
+
+    # save theta evolution
+    trajectory = []
+
+    def retail(theta):
+        "save each step of the training"
+        # Theta = rebuilt(X, theta)
+        # p = predict_multi_class(Theta, X)
+        # p += 1   # y is 1-based index in the examples!
+        # print "Train Accuracy: %f" % (p == y).mean()
+        trajectory.append(theta.copy())
+
+    if load_seed and os.path.exists(FILE_SEED):
+        Theta = np.load(FILE_SEED)
+
+    # create a FNN that match the number of featurres
+    # and 2 hidden layers
+    nn = FNN((features, 100, 30, n_klasses))
+
+    nn.solve(X, y)
+
+    foo = 1
+
+
 if __name__ == '__main__':
-    multiclass_logistic_regression_split()
-    # multiclass_logistic_regression()
+    # multiclass_logistic_regression_k_problems(False)
+    # multiclass_logistic_regression_single_problem(False)
+    multiclass_with_FNN(False)